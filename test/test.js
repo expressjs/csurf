3c449c61606b86d1d35725a82ade46be1f0e07bd
Douglas Christopher Wilson <doug@somethingdoug.com>
2014-06-29 23:35:01 -0400

    tests: minor test refactor

--- test/test.js
+++ test/test.js
@@ -2,28 +2,25 @@
 process.env.NODE_ENV = 'test';
 
 var connect = require('connect');
+var http = require('http')
 var session = require('cookie-session');
 var bodyParser = require('body-parser');
 var cookieParser = require('cookie-parser');
 var request = require('supertest');
 
-var csrf = require('..');
+var csurf = require('..')
 
-describe('csrf', function(){
+describe('csurf', function () {
   it('should work with a valid token (session-based)', function(done) {
-    var app = trifecta();
+    var server = createServer()
 
-    app.use(function(req, res){
-      res.end(req.csrfToken() || 'none');
-    });
-
-    request(app)
+    request(server)
     .get('/')
     .expect(200, function (err, res) {
       if (err) return done(err)
       var token = res.text;
 
-      request(app)
+      request(server)
       .post('/')
       .set('Cookie', cookies(res))
       .set('X-CSRF-Token', token)
@@ -32,20 +29,15 @@ describe('csrf', function(){
   });
 
   it('should work with a valid token (cookie-based, defaults)', function(done) {
-    var app = trifecta(null, { cookie: true });
+    var server = createServer({ cookie: true })
 
-    app.use(function(req, res) {
-      (req.session === undefined).should.be.true;
-      res.end(req.csrfToken() || 'none');
-    });
-
-    request(app)
+    request(server)
     .get('/')
     .expect(200, function (err, res) {
       if (err) return done(err)
       var token = res.text;
 
-      request(app)
+      request(server)
       .post('/')
       .set('Cookie', cookies(res))
       .set('X-CSRF-Token', token)
@@ -54,14 +46,9 @@ describe('csrf', function(){
   });
 
   it('should work with a valid token (cookie-based, custom key)', function(done) {
-    var app = trifecta(null, { cookie: { key: '_customcsrf' } });
+    var server = createServer({ cookie: { key: '_customcsrf' } });
 
-    app.use(function(req, res) {
-      (req.session === undefined).should.be.true;
-      res.end(req.csrfToken() || 'none');
-    });
-
-    request(app)
+    request(server)
     .get('/')
     .expect(200, function (err, res) {
       if (err) return done(err)
@@ -69,7 +56,7 @@ describe('csrf', function(){
 
       res.headers['set-cookie'][0].split('=')[0].should.equal('_customcsrf');
 
-      request(app)
+      request(server)
       .post('/')
       .set('Cookie', cookies(res))
       .set('X-CSRF-Token', token)
@@ -78,17 +65,13 @@ describe('csrf', function(){
   });
 
   it('should fail with an invalid token', function(done) {
-    var app = trifecta();
+    var server = createServer()
 
-    app.use(function(req, res){
-      res.end(req.csrfToken() || 'none');
-    });
-
-    request(app)
+    request(server)
     .get('/')
     .expect(200, function (err, res) {
       if (err) return done(err)
-      request(app)
+      request(server)
       .post('/')
       .set('Cookie', cookies(res))
       .set('X-CSRF-Token', '42')
@@ -97,17 +80,13 @@ describe('csrf', function(){
   });
 
   it('should fail with no token', function(done){
-    var app = trifecta();
+    var server = createServer()
 
-    app.use(function(req, res){
-      res.end(req.csrfToken() || 'none');
-    });
-
-    request(app)
+    request(server)
     .get('/')
     .expect(200, function (err, res) {
       if (err) return done(err)
-      request(app)
+      request(server)
       .post('/')
       .set('Cookie', cookies(res))
       .expect(403, done)
@@ -115,21 +94,27 @@ describe('csrf', function(){
   });
 });
 
-function trifecta(app, opts) {
-  app = app || connect();
-  if (!opts || (opts && !opts.cookie)) {
-    app.use(session({
-      keys: ['a', 'b']
-    }));
-  } else if (opts && opts.cookie)
-    app.use(cookieParser());
-  app.use(bodyParser());
-  app.use(csrf(opts));
-  return app;
-}
-
 function cookies(req) {
   return req.headers['set-cookie'].map(function (cookies) {
     return cookies.split(';')[0];
   }).join(';');
+}
+
+function createServer(opts) {
+  var app = connect()
+
+  if (!opts || (opts && !opts.cookie)) {
+    app.use(session({ keys: ['a', 'b'] }))
+  } else if (opts && opts.cookie) {
+    app.use(cookieParser())
+  }
+
+  app.use(bodyParser())
+  app.use(csurf(opts))
+
+  app.use(function (req, res) {
+    res.end(req.csrfToken() || 'none')
+  })
+
+  return http.createServer(app)
 }
