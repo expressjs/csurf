a9ad8d30442f780f2757e55bd5476cbbfe11f1ab
x24git <23037854+x24git@users.noreply.github.com>
2018-09-06 01:01:32 +0000

    Added Test Cases and fixed conditional errors

--- index.js
+++ index.js
@@ -40,7 +40,6 @@ module.exports = csurf
 
 function csurf (options) {
   var opts = options || {}
-  
 
   // get cookie options
   var cookie = getCookieOptions(opts.cookie)
@@ -71,6 +70,7 @@ function csurf (options) {
     if (!verifyConfiguration(req, sessionKey, cookie)) {
       return next(new Error('misconfigured csrf'))
     }
+
     // get the secret from the request
     var secret = getSecret(req, sessionKey, cookie)
     var token
@@ -89,7 +89,8 @@ function csurf (options) {
       // generate & set new secret
       if (sec === undefined) {
         sec = tokens.secretSync()
-        setSecret(req, res, sessionKey, test, cookie)
+        sec = setExpirey(sec, cookie)
+        setSecret(req, res, sessionKey, sec, cookie)
       }
 
       // update changed secret
@@ -104,16 +105,16 @@ function csurf (options) {
     // generate & set secret
     if (!secret) {
       secret = tokens.secretSync()
+      secret = setExpirey(secret, cookie)
       setSecret(req, res, sessionKey, secret, cookie)
     }
 
     // verify the incoming token
-    if (!ignoreMethod[req.method] && (!tokens.verify(secret, value(req)) || !verifyExpiry(secret,cookie))) {
+    if (!ignoreMethod[req.method] && (!tokens.verify(secret, value(req)) || !verifyExpiry(secret, cookie))) {
       return next(createError(403, 'invalid csrf token', {
         code: 'EBADCSRFTOKEN'
       }))
     }
-
 
     next()
   }
@@ -234,6 +235,25 @@ function getSecretBag (req, sessionKey, cookie) {
 }
 
 /**
+ * Set an expirey time on the cookie.
+ *
+ * @param {string} val
+ * @param {Object} [options]
+ * @api private
+ */
+
+function setExpirey (val, options) {
+  var secret = val
+  if (options) {
+    if (options.maxAge) {
+      var time = ((new Date().getTime() + (options.maxAge * 1000)).toString(21))
+      secret += ('-' + time)
+    }
+  }
+  return secret
+}
+
+/**
  * Set a cookie on the HTTP response.
  *
  * @param {OutgoingMessage} res
@@ -244,12 +264,7 @@ function getSecretBag (req, sessionKey, cookie) {
  */
 
 function setCookie (res, name, val, options) {
-  if (options.maxAge){
-    var time = ((new Date().getTime()+(options.maxAge*1000)).toString(21))
-    val += ("-"+time)
-  }
   var data = Cookie.serialize(name, val, options)
-
   var prev = res.getHeader('set-cookie') || []
   var header = Array.isArray(prev) ? prev.concat(data)
     : Array.isArray(data) ? [prev].concat(data)
@@ -298,19 +313,19 @@ function setSecret (req, res, sessionKey, val, cookie) {
  * Verify the cookie/token has not expired.
  * @private
  */
-function verifyExpiry (secret,cookie) {
-  if(cookie.maxAge){
-    var index = secret.lastIndexOf('-')
-    if (index === -1) 
-      return false
-    var time = secret.substr(index+1,secret.length-index)
-    time = parseInt(time, 21)
-    return (new Date().getTime()) < time
+function verifyExpiry (secret, cookie) {
+  if (cookie) {
+    if (cookie.maxAge) {
+      var index = secret.lastIndexOf('-')
+      if (index === -1) { return false }
+      var time = secret.substr(index + 1, secret.length - index)
+      time = parseInt(time, 21)
+      return (new Date().getTime()) < time
+    }
   }
-  
   return true
-} 
- 
+}
+
 /**
  * Verify the configuration against the request.
  * @private
